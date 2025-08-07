2f7ea04d5a7641b7eb5e9a6584d6d0734b1edacc
Robin <robalexclark@hotmail.com>
2025-08-07 08:38:04 +0100

    Remove navmenu etc

--- PatientApp/Components/Pages/Home.razor.cs
+++ PatientApp/Components/Pages/Home.razor.cs
@@ -1,9 +1,4 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Json;
-using System.Threading.Tasks;
-using Blazorise;
+﻿using Blazorise;
 using Microsoft.AspNetCore.Components;
 using PatientApp.Shared;
 
